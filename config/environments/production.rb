a645a4890dc3b244d7ebdbcc407f0f8d0e719095
hninhninyu95 <hninhninyu1495@gmail.com>
2023-02-23 23:49:21 +0900

    add config host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+	config.hosts << "railscicd-env.eba-gbrxmphr.us-east-1.elasticbeanstalk.com"
+	
   # Code is not reloaded between requests.
   config.cache_classes = true
 
